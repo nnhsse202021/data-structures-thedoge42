cca8b97f995b44e9f55226471f27e2aa6e503932
Geoffrey Wu <gwu@stu.naperville203.org>
2020-09-29 14:57:33 -0500

    wrote the sieve method

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,25 +1,36 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 
 /**
  * A program that implements the sieve of Eratosthenes.
 */
 public class Sieve
 {
-    public static void main(String[] args)
+	public static Set<Integer> sieve(int n) {
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 2; i <= n; i++) {
+			set.add(i);
+		}
+
+		for (int i = 2; i <= n; i++) {
+			if (set.contains(i)) {
+				for (int j = 2*i; j <= n; j += i) {
+					set.remove(j);
+				}
+			}
+		}
+
+		return set;
+	}
+    public static void main(String[] args) 
     {
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
-        int n = in.nextInt();
+		int n = in.nextInt();
 
-        // Your work goes here
-        . . .
+		System.out.println(sieve(n));
 
-
-
-
-
-
-
+		in.close();
     }
 }
