b8134754e11933c9dd13b158c3a074b0c4967e9b
Geoffrey Wu <gwu@stu.naperville203.org>
2020-10-06 23:15:05 -0500

    wrote stringlength class

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -1,57 +1,48 @@
 import java.util.*;
 import java.io.*;
+
 /**
- * Read all words from a file and add them to a
- * map whose keys are word lengths and whose values
- * are comma-separated strings of words of the same length.
- * Then print out those strings, in increasing order by
- * the length of their entries.
- * Modify Worked Example 15.1.
+ * Read all words from a file and add them to a map whose keys are word lengths
+ * and whose values are comma-separated strings of words of the same length.
+ * Then print out those strings, in increasing order by the length of their
+ * entries. Modify Worked Example 15.1.
  */
-public class StringLengthMap
-{
-    public static void main(String[] args) throws FileNotFoundException
-    {
-        String filename = "src/test1.txt";
+public class StringLengthMap {
+	public static void main(String[] args) throws FileNotFoundException {
+		String filename = "src/test1.txt";
 
-        try (Scanner in = new Scanner(new File(filename)))
-        {
+		try (Scanner in = new Scanner(new File(filename))) {
+			Map<Integer, String> wordMap = new TreeMap<>();
 
-            // Create your map here
-            
+			while (in.hasNext()) {
+				String word = clean(in.next());
+				Integer len = word.length();
+				if (wordMap.containsKey(len)) {
+					wordMap.put(len, wordMap.get(len) + ", " + word);
+				} else {
+					wordMap.put(len, word);
+				}
+			}
 
-            while (in.hasNext())
-            {
-                String word = clean(in.next());
-                Integer len = word.length();
+			for (int key : wordMap.keySet()) {
+				System.out.println(key + ": " + wordMap.get(key));
+			}
 
-                // Update the map here
-                // Modify Worked Example 15.1
-                
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot open: " + filename);
+		}
+	}
 
+	public static String clean(String s) {
+		String r = "";
 
-            }
-
-            // Print the strings, in increasing order of their length
-            // Use this format: 1: i, a, i
-        } catch (FileNotFoundException e)
-        {
-            System.out.println("Cannot open: " + filename);
-        }
-    }
-
-    public static String clean(String s)
-    {
-        String r = "";
-        for (int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-            if (Character.isLetter(c))
-            {
-                r = r + c;
-            }
-        }
-        return r.toLowerCase();
-    }
-
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (Character.isLetter(c)) {
+				r = r + c;
+			}
+		}
+		
+		return r.toLowerCase();
+	}
 }
--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap2.java
@@ -1,57 +1,43 @@
 import java.util.*;
 import java.io.*;
+
 /**
- * Read all words from a file and add them to a
- * map whose keys are word lengths and whose values
- * are comma-separated strings of words of the same length.
- * Then print out those strings, in increasing order by
- * the length of their entries.
- * Use the Java 8 merge() method from Java 8 Note 15.1.
+ * Read all words from a file and add them to a map whose keys are word lengths
+ * and whose values are comma-separated strings of words of the same length.
+ * Then print out those strings, in increasing order by the length of their
+ * entries. Use the Java 8 merge() method from Java 8 Note 15.1.
  */
-public class StringLengthMap2
-{
-    public static void main(String[] args)
-    {
-        String filename = "src/test1.txt";
+public class StringLengthMap2 {
+	public static void main(String[] args) throws FileNotFoundException {
+		String filename = "src/test1.txt";
 
-        try (Scanner in = new Scanner(new File(filename)))
-        {
+		try (Scanner in = new Scanner(new File(filename))) {
 
-            // Create your map here
-            
+			Map<Integer, String> wordMap = new TreeMap<>();
 
-            while (in.hasNext())
-            {
-                String word = clean(in.next());
-                Integer len = word.length();
+			while (in.hasNext()) {
+				String word = clean(in.next());
+				Integer len = word.length();
+				wordMap.merge(len, word, (oldString, newWord) -> oldString + ", " + newWord);
+			}
 
-                // Update the map here
-                // Use the Java 8 merge() method
-                
+			for (int key : wordMap.keySet()) {
+				System.out.println(key + ": " + wordMap.get(key));
+			}
 
+		} catch (FileNotFoundException e) {
+			System.out.println("Cannot open: " + filename);
+		}
+	}
 
-            }
-
-            // Print the strings, in increasing order of their length
-            // Use this format: 1: i, a, i
-        } catch (FileNotFoundException e)
-        {
-            System.out.println("Cannot open: " + filename);
-        }
-    }
-
-    public static String clean(String s)
-    {
-        String r = "";
-        for (int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-            if (Character.isLetter(c))
-            {
-                r = r + c;
-            }
-        }
-        return r.toLowerCase();
-    }
-
+	public static String clean(String s) {
+		String r = "";
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (Character.isLetter(c)) {
+				r = r + c;
+			}
+		}
+		return r.toLowerCase();
+	}
 }
