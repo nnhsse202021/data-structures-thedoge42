133875f7e9c2df88851bdc791a37ab86520dded4
Geoffrey Wu <gwu@stu.naperville203.org>
2020-10-15 15:04:09 -0500

    wrote driveway class

--- Chapter 15 Activities/Driveway/src/Driveway.java
+++ Chapter 15 Activities/Driveway/src/Driveway.java
@@ -1,70 +1,60 @@
 import java.util.Stack;
-import java.util.Scanner;
 
 /**
- * Class for simulating a driveway and a street, using stacks
- * of cars with license plate numbers as representation.
-*/
-public class Driveway
-{
+ * Class for simulating a driveway and a street, using stacks of cars with
+ * license plate numbers as representation.
+ */
+public class Driveway {
     /**
-      * Stack representing the cars in the driveway.
-    */
+     * Stack representing the cars in the driveway.
+     */
     private Stack<Integer> driveway;
+
     /**
-      * Stack representing the cars in the street.
-    */
+     * Stack representing the cars in the street.
+     */
     private Stack<Integer> street;
 
     /**
-      * Constructor.
-    */
-    public Driveway()
-    {
-        // Complete the constructor
-        ...
-
-
+     * Constructor.
+     */
+    public Driveway() {
+        this.driveway = new Stack<>();
+        this.street = new Stack<>();
     }
 
     /**
-      * Add the given license plate to the driveway.
-      *
-      * @param licensePlate number of license plate.
-    */
-    public void add(int licensePlate)
-    {
-        // Complete this method
-        ...
-
-
+     * Add the given license plate to the driveway.
+     *
+     * @param licensePlate number of license plate.
+     */
+    public void add(int licensePlate) {
+        driveway.push(licensePlate);
+        print();
     }
 
     /**
-      * Remove the given license plate from the driveway.
-      *
-      * @param licensePlate number of license plate.
-    */
-    public void remove(int licensePlate)
-    {
-        // Complete this method
-        ...
-
-
+     * Remove the given license plate from the driveway.
+     *
+     * @param licensePlate number of license plate.
+     */
+    public void remove(int licensePlate) {
+        int plate = driveway.pop();
+        while (licensePlate != plate) {
+            street.add(plate);
+            plate = driveway.pop();
+        }
+        print();
     }
 
     /**
-      * Prints the driveway and street details to the screen.
-    */
-    public void print()
-    {
-        System.out.println("In Driveway, starting at first in (one license plate per line):");
-        // Print the cars in the driveway here
-        ...
+     * Prints the driveway and street details to the screen.
+     */
+    public void print() {
+        System.out.println("In Driveway, starting at first in:");
+        System.out.println(this.driveway);
 
-        System.out.println("In Street, starting at first in (one license plate per line):");
-        // Print the cars in the street here
-        ...
-
+        System.out.println("In Street, starting at first in:");
+        System.out.println(this.street);
     }
 }
--- Chapter 15 Activities/Driveway/src/DrivewayDemo.java
+++ Chapter 15 Activities/Driveway/src/DrivewayDemo.java
@@ -1,29 +1,21 @@
 import java.util.Scanner;
 
-public class DrivewayDemo
-{
-    public static void main(String[] args)
-    {
+public class DrivewayDemo {
+    public static void main(String[] args) {
         Driveway testDriveway = new Driveway();
         Scanner in = new Scanner(System.in);
         System.out.println("Positive integers add cars to the driveway");
         System.out.println("Negative license numbers remove cars.");
         System.out.println("0 stops the simulation.");
 
-        while (in.hasNextInt())
-        {
+        while (in.hasNextInt()) {
             int licensePlate = in.nextInt();
-            if (licensePlate > 0)
-            {
+            if (licensePlate > 0) {
                 testDriveway.add(licensePlate);
-            }
-            else if (licensePlate < 0)
-            {
-                testDriveway.remove(0 - licensePlate);
-            }
-            else
-            {
-            	testDriveway.print();
+            } else if (licensePlate < 0) {
+                testDriveway.remove(-licensePlate);
+            } else {
+                testDriveway.print();
                 return;
             }
         }
